6f3456337842804f92baf2c8259450f5200254d3
vanessa <20596550+wordlesspirate@users.noreply.github.com>
2020-04-06 15:35:17 +0100

    This will add markers to map; the initalCenter marker has a different color

--- src/Components/MovieMap.jsx
+++ src/Components/MovieMap.jsx
@@ -1,8 +1,31 @@
 import React, { Component } from "react";
-import { Map, GoogleApiWrapper } from "google-maps-react";
+import { Map, GoogleApiWrapper, Marker } from "google-maps-react";
 import APIKey from "../config";
 
 class MovieMap extends Component {
+  state = {
+    movieLocations: [
+      { lat: 53.955755, lng: -1.078316 },
+      { lat: 53.959446, lng: -1.080061 },
+    ],
+  };
+
+  displayMarkers = () => {
+    return this.state.movieLocations.map((movieLocation, index) => {
+      return (
+        <Marker
+          key={index}
+          id={index}
+          position={{
+            lat: movieLocation.lat,
+            lng: movieLocation.lng,
+          }}
+          onClick={() => console.log("You clicked me!")}
+        />
+      );
+    });
+  };
+
   render() {
     return (
       <div>
@@ -11,7 +34,15 @@ class MovieMap extends Component {
           zoom={8}
           style={mapStyles}
           initialCenter={{ lat: 53.960192, lng: -1.092438 }}
-        />
+        >
+          <Marker
+            position={{ lat: 53.960192, lng: -1.092438 }}
+            icon={{
+              url: "http://maps.google.com/mapfiles/ms/icons/blue-dot.png",
+            }}
+          />
+          {this.displayMarkers()}
+        </Map>
       </div>
     );
   }
