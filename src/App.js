57ffe772536683eea06c32df4d746b49c69a5d30
Philippa <philippa.claire9@gmail.com>
2020-04-07 16:09:11 +0100

    continue to build route functionality

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import React, { Component } from "react";
-import "./App.css";
-import MovieMap from "./Components/MovieMap";
+import React, { Component } from 'react';
+import './App.css';
+//import MovieMap from './Components/MovieMap';
+import WrappedMap from './Components/MovieMap';
+import APIKey from './config';
+import Header from './Header';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <MovieMap />
-      </div>
+      <>
+        <Header />
+        <div style={{ width: '75%', height: '100vh' }}>
+          <WrappedMap
+            googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${APIKey}`}
+            loadingElement={<div style={{ height: '100' }} />}
+            containerElement={<div style={{ height: '100%' }} />}
+            mapElement={<div style={{ height: '100%' }} />}
+          />
+
+          {/*<MovieMap /> */}
+        </div>
+      </>
     );
   }
 }
---
+++ src/Header.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Header = () => {
+  return <h1>Movie Locations Map</h1>;
+};
+
+export default Header;
