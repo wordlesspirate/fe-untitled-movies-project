91c0b28645d9d8b3ddc363ccce3d136d58fd18c7
vanessa <20596550+wordlesspirate@users.noreply.github.com>
2020-04-08 14:20:41 +0100

    This will use Google Geocoding to transform addresses into coordinates

---
+++ src/Utils/api.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+import APIKey from "../config";
+
+export const getLatLng = (address) => {
+  return axios
+    .get(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${APIKey}`
+    )
+    .then(({ data: { results } }) => {
+      return results[0].geometry.location;
+    });
+};
+
+// https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=YOUR_API_KEY
