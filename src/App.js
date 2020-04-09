1389b3ecd3726d7cde4c907debcbc0e409971989
Cooki3 <pgcdigit@gmail.com>
2020-04-09 16:26:44 +0100

    login auth removed

--- src/App.js
+++ src/App.js
@@ -9,37 +9,7 @@ import { Auth } from "aws-amplify";
 import Home from "./Components/Home";
 
 class App extends Component {
-  state = {
-    isAuthenticated: false,
-    isAuthenticating: true,
-    user: null,
-  };
-
-  // setAuthStatus = (authenticated) => {
-  //   console.log(authenticated);
-  //   this.setState({ isAuthenticated: authenticated });
-  // };
-
-  // setUser = (user) => {
-  //   this.setState({ user: user });
-  // };
-
-  // async componentDidMount() {
-  //   try {
-  //     const session = await Auth.currentSession();
-  //     this.setAuthStatus(true);
-  //     console.log("doing this bit");
-  //     console.log(session);
-  //     const user = await Auth.currentAuthenticatedUser();
-  //     this.setUser(user);
-  //   } catch (error) {
-  //     if (error !== "No current user") {
-  //       console.log(error);
-  //     }
-  //   }
-
-  //   this.setState({ isAuthenticating: false });
-  // }
+  state = {};
 
   render() {
     return (
