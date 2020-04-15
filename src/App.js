b93d2eb781bcbbfc4d39b841063f7839928fa6ac
JlBos24 <jlbosman24@gmail.com>
2020-04-15 10:35:26 +0100

    profiles feature

--- src/App.js
+++ src/App.js
@@ -8,41 +8,38 @@ import Header from "./Header";
 import LocationMarkers from "./Components/LocationMarkers";
 import MovieResponse from "./Components/MovieResponse";
 import Dashboard from "./Components/Dashboard";
-
+import Home from "./Components/Home";
 import MovieMap from "./Components/MovieMap";
 import Register from "./Components/Register";
 import Welcome from "./Components/Welcome";
 import Login from "./Components/Login";
+import Genres from "./Components/Genres";
 import { Router } from "@reach/router";
 import { Auth } from "aws-amplify";
-import Home from "./Components/Home";
-
+import Profile from "./Components/Profile";
 
 class App extends Component {
   state = {};
 
   render() {
     return (
-
-      <>
-        {/* <Dashboard /> */}
-
-        <Dashboard />
-        <MovieResponse />
-        {/*<MovieMap /> */}
-        {/* <LocationMarkers /> */}
-      </>
-
-      <div>
+      <div className={"AppMain"}>
+        <Home />
         <Router>
-          <MovieMap path="/map" />
+          <Profile path="/profile/*" />
+          <Genres path="/profile/genres" />
+          {/* <Genres path="/genres" /> */}
+
+          {/* <MovieMap path="/map" /> */}
+          {/* <LocationMarkers /> */}
+          {/* <Dashboard /> */}
+          {/* {<MovieResponse /> */}
         </Router>
+
         <Register />
         <Welcome />
         <Login />
-        <Home />
       </div>
-
     );
   }
 }
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,8 +1,18 @@
 import React, { Component } from "react";
+import { Link } from "@reach/router";
 
 class Home extends Component {
   render() {
-    return <div>WELCOME</div>;
+    return (
+      <main>
+        WELCOME
+        <div>
+          <Link to="/profile">
+            <button>My Profile</button>
+          </Link>
+        </div>
+      </main>
+    );
   }
 }
 
