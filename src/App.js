5eb330c22b2f124f76dbd6733a1feff05e795c97
JlBos24 <jlbosman24@gmail.com>
2020-04-15 11:41:44 +0100

    review1

--- src/App.js
+++ src/App.js
@@ -1,47 +1,18 @@
 import React, { Component } from "react";
-// import "./App.css";
+import "./App.css";
 
-
-// //import MovieMap from './Components/MovieMap';
-// import WrappedMap from "./Components/MovieMap";
-// import APIKey from "./config";
-// import Header from "./Header";
-// import LocationMarkers from "./Components/LocationMarkers";
-// import MovieResponse from "./Components/MovieResponse";
-// import Dashboard from "./Components/Dashboard";
 import SimpleReactLightbox from "simple-react-lightbox";
-// import MovieMap from "./Components/MovieMap";
-
-//import MovieMap from './Components/MovieMap';
-// import WrappedMap from "./Components/MovieMap";
-// import APIKey from "./config";
-// import Header from "./Components/Header";
-// import LocationMarkers from "./Components/LocationMarkers";
-import MovieResponse from "./Components/MovieResponse";
 import Dashboard from "./Components/Dashboard";
-
-// import UserLocation from "./Components/UserLocation";
-
-// import MovieMap from "./Components/MovieMap";
-// import Register from "./Components/Register";
-// import Welcome from "./Components/Welcome";
-// import Login from "./Components/Login";
-// import { Router } from "@reach/router";
-// import { Auth } from "aws-amplify";
-// import Home from "./Components/Home";
-
 import Home from "./Components/Home";
-import MovieMap from "./Components/MovieMap";
-
 import Register from "./Components/Register";
-// import Welcome from "./Components/Welcome";
 import Login from "./Components/Login";
+import Gallery from "./Components/Gallery";
+import Usercamera from "./Components/Usercamera";
+import Genres from "./Components/Genres";
+import Profile from "./Components/Profile";
 
 import { Router, Link } from "@reach/router";
 import { Auth } from "aws-amplify";
-import Home from "./Components/Home";
-import Gallery from "./Components/Gallery";
-import Usercamera from "./Components/Usercamera";
 
 class App extends Component {
   state = {
@@ -77,7 +48,7 @@ class App extends Component {
       setAuthenticated: this.setAuthStatus,
       userInfo: this.setUser,
     };
-    console.log(SimpleReactLightbox);
+
     return (
       !this.state.isAuthenticating && (
         <div>
@@ -87,74 +58,28 @@ class App extends Component {
             <Link to="/home">Home</Link>
             <Link to="/userCamera">Camera</Link>
             <Link to="/gallery">Gallery</Link>
+            <Link to="/profile">My Profile</Link>
+            <Link to="/maps">Dashboard</Link>
           </nav>
           <Router>
             <Login auth={authProps} path="/" />
+
             <Register
               userProps={this.props}
               auth={authProps}
               path="/register"
             />
-            <Home auth={authProps} path="home" />
-            <Usercamera auth={authProps} path="userCamera" />
-            <Gallery auth={authProps} path="gallery" />
+            <Home auth={authProps} path="/home" />
+            <Usercamera auth={authProps} path="/userCamera" />
+            <Gallery auth={authProps} path="/gallery" />
+            <Dashboard auth={authProps} path="/maps" />
+            <Profile auth={authProps} path="/profile/*" />
+            <Genres auth={authProps} path="/profile/genres" />
           </Router>
           <div></div>
         </div>
       )
-import Genres from "./Components/Genres";
-import { Router } from "@reach/router";
-import { Auth } from "aws-amplify";
-import Profile from "./Components/Profile";
-
-
-class App extends Component {
-  state = {};
-
-  render() {
-    return (
-
-      <>
-        {/* <Dashboard /> */}
-
-        <Dashboard />
-        <MovieResponse />
-        {/*<MovieMap /> */}
-        {/* <LocationMarkers /> */}
-
-        {/* <div>
-          <Router>
-            <MovieMap path="/map" />
-          </Router>
-          <Register />
-          <Welcome />
-          <Login />
-          <Home />
-        </div> */}
-      </>
-
-      <div className={"AppMain"}>
-        <Home />
-        <Router>
-          <Profile path="/profile/*" />
-          <Genres path="/profile/genres" />
-          {/* <Genres path="/genres" /> */}
-
-          {/* <MovieMap path="/map" /> */}
-          {/* <LocationMarkers /> */}
-          {/* <Dashboard /> */}
-          {/* {<MovieResponse /> */}
-        </Router>
-
-        <Register />
-        <Welcome />
-        <Login />
-
-      </div>
-
-
     );
   }
 }
-
 export default App;
