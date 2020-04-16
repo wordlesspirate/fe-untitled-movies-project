bf86b4651e1448a3c02d2aa8ec39e0f946c3ed12
Philippa <philippa.claire9@gmail.com>
2020-04-16 09:00:43 +0100

    complete waypoint functionality

--- src/Components/RouteCalculator.jsx
+++ src/Components/RouteCalculator.jsx
@@ -1,11 +1,11 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import {
   // GoogleMap,
   // Marker,
   // DirectionsService,
   DirectionsRenderer,
   // LatLng,
-} from "react-google-maps";
+} from 'react-google-maps';
 // import APIKey from "../config";
 
 class RouteCalculator extends Component {
@@ -27,7 +27,8 @@ class RouteCalculator extends Component {
       this.props.destination !== prevProps.destination ||
       this.props.stops !== prevProps.stops
     ) {
-      console.log("updating?", this.props.stops);
+      this.setState({ directions: null });
+      console.log('updating?', this.props.stops);
       const DirectionsService = new window.google.maps.DirectionsService();
 
       DirectionsService.route(
@@ -40,7 +41,6 @@ class RouteCalculator extends Component {
             this.props.destination.lat,
             this.props.destination.lng
           ),
-          // waypoints: this.props.stops,
 
           waypoints: this.props.stops.map((stop) => {
             // console.log(stop.lat);
@@ -48,89 +48,45 @@ class RouteCalculator extends Component {
               location: new window.google.maps.LatLng(stop.lat, stop.lng),
             };
           }),
-          travelMode: "DRIVING",
+          travelMode: 'DRIVING',
+          optimizeWaypoints: true,
         },
         (result, status) => {
           // console.log("this is steps", result.routes[0].legs[0].steps);
 
-          if (status === "OK") {
+          if (status === window.google.maps.DirectionsStatus.OK) {
             this.setState(
               {
-                directions: result,
+                directions: { ...result },
                 // polyline: result.routes[0].overview_polyline,
               },
               () => {
-                console.log("this is result", result);
+                console.log(
+                  'state>>>>',
+                  this.state.directions,
+                  'result>>>>',
+                  result
+                );
               }
             );
           } else {
-            console.dir("console.dir", result);
+            console.dir('console.dir', result);
           }
         }
       );
     }
-    // if (this.props.stops !== prevProps.stops) {
-    //   console.log("updating?", this.props.stops);
-    //   const DirectionsService = new window.google.maps.DirectionsService();
-
-    //   DirectionsService.route(
-    //     {
-    //       origin: new window.google.maps.LatLng(
-    //         this.state.userLocation.lat,
-    //         this.state.userLocation.lng
-    //       ),
-    //       destination: new window.google.maps.LatLng(
-    //         this.props.destination.lat,
-    //         this.props.destination.lng
-    //       ),
-    //       waypoints: this.props.stops.map((stop) => {
-    //         console.log(stop.lat);
-    //         return {
-    //           location: new window.google.maps.LatLng(stop.lat, stop.lng),
-    //         };
-    //       }),
-    //       travelMode: "DRIVING",
-    //     },
-    //     (result, status) => {
-    //       // console.log("this is steps", result.routes[0].legs[0].steps);
-
-    //       if (status === "OK") {
-    //         this.setState(
-    //           {
-    //             directions: result,
-    //             polyline: result.routes[0].overview_polyline,
-    //           },
-    //           () => {
-    //             console.log("investigating polyline", result);
-    //           }
-    //         );
-    //       } else {
-    //         console.dir("console.dir", result);
-    //       }
-    //     }
-    //   );
-    // }
   }
 
   render() {
     return (
       <div>
-        {" "}
+        {' '}
         {this.state.directions && (
           <DirectionsRenderer defaultDirections={this.state.directions} />
-        )}{" "}
+        )}{' '}
       </div>
     );
   }
 }
 
 export default RouteCalculator;
-
-// waypoints: [
-//   {
-//     location: new google.maps.LatLng(14.546748, 121.05455),
-//   },
-//   {
-//     location: new google.maps.LatLng(14.552444, 121.044488),
-//   },
-// ];
